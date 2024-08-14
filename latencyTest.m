25d3c452d1f917bdfd0cf3261cdf4ea448319a9a
Jaxon Mitchell <jaxon.mitchell@flinders.edu.au>
2024-08-14 15:08:05 +0930

    Made formatting a bit easier to diagnose and edit

--- latencyTest.m
+++ latencyTest.m
@@ -9,10 +9,15 @@ useAllFrames = true;
 videos = ["/home/hoverfly/Documents/RE-Color Fill_14-Aug-2024 13_24_46.mp4", ...
    "/home/hoverfly/Documents/RE-Color Fill_14-Aug-2024 13_24_54.mp4", ...
    "/home/hoverfly/Documents/RE-Color Fill_14-Aug-2024 13_25_01.mp4"];
+% Threshold values to determine when stimuli changes
+luminence1 = 18;
+luminence2 = 25;
+luminence3 = 20;
+
 for currentVideo = 1:length(videos)
    disp("% %%%%%% %")
    disp("THE FOLLOWING INFO IS FOR VIDEO:")
-   disp(currentVideo)
+   disp(videos(currentVideo))
    disp("% %%%%%% %")
    % Load the video and extract the frames from it
    video = VideoReader(videos(currentVideo));
@@ -33,17 +38,17 @@ for currentVideo = 1:length(videos)
        B = frame(1,320,3);
        avLuminence(currentFrame) = (R + G + B) / 3;
        % If the luminence has gone down, mark sideslip beginning
-       if flag == 0 && avLuminence(currentFrame) < 18
+       if flag == 0 && avLuminence(currentFrame) < luminence1
            flag = 1;
            disp("The first frame where the stimulus starts is:")
            disp(currentFrame)
        end
-       if flag == 1 && avLuminence(currentFrame) > 25
+       if flag == 1 && avLuminence(currentFrame) > luminence2
            flag = 2;
            disp("The first frame where the screen goes white is:")
            disp(currentFrame)
        end
-       if flag == 2 && avLuminence(currentFrame) < 20
+       if flag == 2 && avLuminence(currentFrame) < luminence3
            flag = 3;
            disp("The screen goes black again at frame:")
            disp(currentFrame)
